e232ca412e1cf64128c76c82779ab846b6e2f448
lfsmoura <leofsmoura@gmail.com>
2020-12-05 15:32:45 -0300

    Solve 1 and 2

---
+++ p01/a.clj
@@ -0,0 +1,57 @@
+(use 'clojure.java.io)
+(use 'clojure.test)
+
+(defn tap [val] 
+    (do (println val) val))
+
+(defn parse-int [x] (Integer/parseInt x))
+
+(def example (list 1721 979 366 299 675 1456))
+
+(defn find-two-that-sum
+    {:test #(do
+        (is (= (set (find-two-that-sum example 2020)) (set '(1721 299))))
+    )}
+    [ls num]
+    (let [hashset (if (set? ls) ls (set ls))] 
+        (reduce 
+            (fn [sol elem] (cond
+                (not (nil? sol)) sol  
+                (.contains hashset (- num elem)) (list elem (- num elem))
+                :else nil))
+            nil ls)
+    )
+)
+
+(test #'find-two-that-sum)
+
+(defn find-three-that-sum
+    {:test #(do 
+        (is (= (set (find-three-that-sum example 2020)) (set (list 979 366 675))))
+    )}
+    [ls num]
+    (let [hashset (if (set? ls) ls (set ls))] 
+        (reduce
+            (fn [sol elem] 
+                (if (nil? sol)
+                    (let [two-that-sum (find-two-that-sum ls (- num elem))]
+                        (if (nil? two-that-sum)
+                        nil
+                        (cons elem two-that-sum))
+                    )
+                    sol
+                )
+            )
+            nil
+            ls
+        )
+    )
+)
+
+(test #'find-three-that-sum)
+
+(def input (with-open [rdr (reader "input.txt")]
+    (map parse-int (doall (line-seq rdr) ))))
+
+(println "Solution 1:" (apply * (find-two-that-sum input 2020)))
+(println "Solution 2:" (apply * (find-three-that-sum input 2020)))
---
+++ p02/a.clj
@@ -0,0 +1,88 @@
+(use 'clojure.java.io)
+(use 'clojure.test)
+
+(defn tap [val] 
+    (do (println val) val))
+
+(defn parse-int [x] (Integer/parseInt x))
+
+(def example1 "1-3 a: abcde")
+(def example2 "1-3 b: cdefg")
+(def example3 "4-18 r: rrrdrrrrrrrrrkblrr")
+(def example4 "2-9 c: ccccccccc")
+
+(defn parse-input
+    {:test #(do
+        (is (= (parse-input example1) (list 1 3 "a" "abcdefeeeeeeeee")))
+        (is (= (parse-input example3) (list 4 18 "r" "rrrdrrrrrrrrrkblrr")))
+    )}
+    [str]
+    (rest (re-find #"(\d+)\-(\d+) ([a-z])\: ([a-z]*)" str))
+)
+
+;;(test #'parse-input)
+
+(defn count-occurrences
+    {:test #(do
+        (is (= (count-occurrences \a "bbabba") 2))
+    )}
+    [letter input]
+        (reduce
+            (fn [sum x] (if (= x letter) (+ sum 1) sum))
+            0
+            (seq input)
+        )
+    )
+
+(test #'count-occurrences)
+
+(defn valid
+    {:test #(do
+        (is (= (valid (parse-input example1)) 1))
+        (is (= (valid (parse-input example2)) 0))
+    )}
+    [password]
+        (let [[min max letter input] password
+              char-letter (first (seq letter))
+              count (count-occurrences char-letter input)
+              res (list count input)]
+            (if (and (>= count (parse-int min)) (<= count (parse-int max))) 1 0)
+        )
+    )
+
+(test #'valid)
+
+(def input (with-open [rdr (reader "input.txt")]
+    (map parse-input (doall (line-seq rdr)))))
+
+(println "Solution 1:" (reduce + (map valid input)))
+
+(defn xor
+    [a b]
+    (or (and a (not b))
+        (and (not a) b)
+    ))
+
+(defn valid2
+    {:test #(do
+        (is (= (valid2 (parse-input example1)) 1))
+        (is (= (valid2 (parse-input example2)) 0))
+        (is (= (valid2 (parse-input example4)) 0))
+    )}
+        [password]
+        (let [[i j letter input] password
+            char-letter (first (seq letter))
+            ]
+            (if (xor 
+                    (= char-letter (nth input (- (parse-int i) 1)))
+                    (= char-letter (nth input (- (parse-int j) 1)))
+                )
+                1
+                0
+            )
+        )
+    )
+
+(test #'valid2)
+
+(println "Solution 2:" (reduce + (map valid2 input)))
